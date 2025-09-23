a33569b4d8f85fa3151f21cf14179707e977e846
James <jlbaker361@gmail.com>
2025-09-23 19:31:19 -0400

    maybe?

--- evaluation.py
+++ evaluation.py
@@ -15,6 +15,9 @@ import wandb
 from style_rl.eval_helpers import DinoMetric
 from transformers import AutoProcessor, CLIPModel
 import ImageReward as RM
+from src.customized_pipe import TI2I_StableDiffusion3Pipeline
+from src.attn_processor import TI2I_JointAttnProcessor2_0_multi
+
 
 parser=argparse.ArgumentParser()
 
@@ -27,37 +30,74 @@ parser.add_argument("--project_name",type=str,default="baseline")
 parser.add_argument("--limit",type=int,default=-1)
 parser.add_argument("--size",type=int,default=256)
 parser.add_argument("--background",action="store_true")
+parser.add_argument("--object",type=str, default="person")
+parser.add_argument("--size",type=int,default=256)
 
 
-@torch.no_grad()
-def main(args):
-    ir_model=RM.load("ImageReward-v1.0")
-        
-        
-    clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-    processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
-    accelerator=Accelerator(log_with="wandb",mixed_precision=args.mixed_precision)
-    accelerator.init_trackers(project_name=args.project_name,config=vars(args))
-
-    dino_metric=DinoMetric(accelerator.device)
 
 def main(args):
-    #ir_model=RM.load("ImageReward-v1.0")
-        
-        
-    clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-    processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
     accelerator=Accelerator(log_with="wandb",mixed_precision=args.mixed_precision)
     accelerator.init_trackers(project_name=args.project_name,config=vars(args))
-
-    dino_metric=DinoMetric(accelerator.device)
-
     torch_dtype={
         "no":torch.float32,
         "fp16":torch.float16,
         "bf16":torch.bfloat16
     }[args.mixed_precision]
     device=accelerator.device
+    #ir_model=RM.load("ImageReward-v1.0")
+    pipe = TI2I_StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large",
+                                                            torch_dtype=torch_dtype,
+                                                            device_map="balanced")
+
+    layer_count = 0
+    operator="concat"
+    attn_processors = []
+    def iter_net(net):
+        global layer_count
+        for child in net.children():
+            if "Attention" in child.__class__.__name__:
+                child.processor = TI2I_JointAttnProcessor2_0_multi(contextual_replace=True, operator=operator,
+                wta_control_signal={"on":False},
+                ref_control_signal={"on":False})
+                attn_processors.append(child.processor)
+                layer_count += 1
+            iter_net(child)
+
+    if args.background:
+        def iter_net(net):
+            global layer_count
+            for child in net.children():
+                if "Attention" in child.__class__.__name__:
+                    child.processor = TI2I_JointAttnProcessor2_0_multi(layer=layer_count, contextual_replace=True,
+                                                                    wta_control_signal={"on":True,
+                                                                                        "hyper_parameter":{
+                                                                                                        "wta_weight":[1, 1, 1,1],
+                                                                                                        "cross2ref":True,
+                                                                                                        "wta_shift":[0, 0, 0,0],
+                                                                                                        "wta_cross":False
+                                                                                                        },
+                                                                                        "debug":False},
+                                                                    ref_control_signal={"on":True,
+                                                                                        "ref_idxs":[0,1,2,3], 
+                                                                                        "control_type":"main_context",
+                                                                                        "debug":False,
+                                                                                        "control_layers":[i for i in range(25,40)],
+                                                                                        "hyper_parameter":{}},
+                                                                                        )
+                    attn_processors.append(child.processor)
+                    layer_count += 1
+                iter_net(child)
+
+    iter_net(pipe.transformer)
+        
+        
+    clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    
+
+    dino_metric=DinoMetric(accelerator.device)
+
+    
 
     
 
@@ -72,16 +112,70 @@ def main(args):
     #ir_score_list=[]
     dino_score_list=[]
 
-    
-
+    if args.background:
+        from quad_entry import convert_to_raimg_prompt
+    else:
+        from single_entry import convert_to_raimg_prompt
 
     for k,row in enumerate(data):
         if k==args.limit:
             break
         
+        object=args.object
+        if "object" in row:
+            object=row["object"]
         prompt=real_test_prompt_list[k%len(real_test_prompt_list)]
         background_image=background_dict[prompt]
         image=row["image"]
+        main_prompt=object+" "+prompt
+        if args.background:
+            sub_prompt=[object, prompt]
+            image=[image, background_image]
+        else:
+            sub_prompt=[main_prompt]
+            image=[image]
+
+
+        augmented_image = pipe.img2img_multi(
+            images=image,
+            prompt_a=main_prompt,
+            prompt_b=sub_prompt,
+            negative_prompt="",
+            num_inference_steps=28,
+            guidance_scale=3.5,
+            strength=1,
+            height=args.size,
+            width=args.size,
+        )
+
+        concat=concat_images_horizontally([image,augmented_image])
+
+        accelerator.log({
+            f"image_{k}":wandb.Image(concat)
+        })
+        with torch.no_grad():
+            inputs = processor(
+                    text=[prompt], images=[image,augmented_image,background_image], return_tensors="pt", padding=True
+            )
+
+            outputs = clip_model(**inputs)
+            image_embeds=outputs.image_embeds.detach().cpu()
+            text_embeds=outputs.text_embeds.detach().cpu()
+            logits_per_text=torch.matmul(text_embeds, image_embeds.t())[0]
+        #accelerator.print("logits",logits_per_text.size())
+
+        image_similarities=torch.matmul(image_embeds,image_embeds.t()).numpy()[0]
+
+        [_,text_score,__]=logits_per_text
+        [_,image_score,image_score_background]=image_similarities
+        #ir_score=ir_model.score(prompt,augmented_image)
+        dino_score=dino_metric.get_scores(image, [augmented_image])
+
+        text_score_list.append(text_score.detach().cpu().numpy())
+        image_score_list.append(image_score)
+        image_score_background_list.append(image_score_background)
+        #ir_score_list.append(ir_score)
+        dino_score_list.append(dino_score)
 
        
     accelerator.log({
